b366311a3ba1eb24c1f00bd6e1bde75692a75cb6
antoinem <massartantoine6@gmail.com>
2023-01-18 14:53:30 +0100

    Vibz 1.3 (1ère itération fin)

--- vibz/script.js
+++ vibz/script.js
@@ -1,77 +1,189 @@
+// pin le background de la section artiste
+
 gsap.to("#artistes", {
-  //   scrollTrigger: {
-  //     trigger: "#artistes",
-  //     // afficher les marqueurs de ScrollTrigger
-  //     // utile pour débugger
-  //     // position des marqueurs
-  //     start: "top bottom",
-  //     end: "bottom top",
-  //     // pin: true,
-  //     // synchroniser avec le scroll
-  //   },
+  scrollTrigger: {
+    trigger: "#artistes",
+    start: "top top",
+    end: "bottom 70%",
+    pin: true,
+  },
 });
 
-const dancerScrollTriggerConfiguration = {
-  trigger: "#artistes",
-  // afficher les marqueurs de ScrollTrigger
-  // utile pour débugger
+// creation de la timeline des portraits
 
-  // position des marqueurs
-  start: "top 90%",
-  end: "bottom 100%",
-  // synchroniser avec le scroll
-  scrub: 1,
-};
-
-gsap.from("#dancer1", {
-  x: -600,
-  y: -600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
+const portraitsTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#artistes",
+    start: "top top",
+    end: "bottom 80%",
+    scrub: 1,
+  },
 });
 
-gsap.from("#dancer2", {
-  x: 0,
-  y: -600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
+//FromTo pour chaque animation de la timeline
+
+portraitsTimeline.fromTo(
+  "#dancer1",
+  {
+    x: -600,
+    y: -600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer2",
+  {
+    x: 0,
+    y: -600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer3",
+  {
+    x: 600,
+    y: -600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer4",
+  {
+    x: -600,
+    y: 0,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer5",
+  {
+    x: 600,
+    y: 0,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer6",
+  {
+    x: -600,
+    y: 600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer7",
+  {
+    x: 0,
+    y: 600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#dancer8",
+  {
+    x: 600,
+    y: 600,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
+
+portraitsTimeline.fromTo(
+  "#final",
+  {
+    scale: 0,
+  },
+  {
+    scale: 1,
+  }
+);
+
+// pin le background de la section price
+
+gsap.to("#price", {
+  scrollTrigger: {
+    trigger: "#price",
+    start: "top top",
+    end: "bottom 75%",
+    pin: true,
+  },
 });
 
-gsap.from("#dancer3", {
-  x: 600,
-  y: -600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
+// timeline des deplacements des blocks de prix
+
+const priceTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#price",
+    start: "top top",
+    end: "bottom 80%",
+    scrub: 1,
+  },
 });
 
-gsap.from("#dancer4", {
-  x: -600,
-  y: 0,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
+// mouvement to des blocks
 
-gsap.from("#dancer5", {
-  x: 600,
-  y: 0,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
+priceTimeline.to(
+  "#annual",
+  {
+    x: 300,
+    y: 54,
+  },
+  0
+);
 
-gsap.from("#dancer6", {
-  x: -600,
-  y: 600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
+priceTimeline.to(
+  "#monthly",
+  {
+    x: -300,
+    y: -274,
+  },
+  0
+);
 
-gsap.from("#dancer7", {
-  x: 0,
-  y: 600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
+priceTimeline.to(
+  "#annual",
+  {
+    x: 0,
+    y: 274,
+  },
+  1
+);
 
-gsap.from("#dancer8", {
-  x: 600,
-  y: 600,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
-
-gsap.from("#final", {
-  scale: 0,
-  scrollTrigger: dancerScrollTriggerConfiguration,
-});
+priceTimeline.to(
+  "#monthly",
+  {
+    x: 0,
+    y: -404,
+  },
+  1
+);
